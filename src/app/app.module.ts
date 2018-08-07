d40b2035c8d3eb1c5c41fbbc735bdf73e6c3bbf7
Khairul <khairulice@gmail.com>
2018-08-07 20:29:52 +0600

    Reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { routing } from './app.routing';
-import { FormsModule }   from '@angular/forms';
+import { FormsModule,ReactiveFormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
@@ -22,6 +22,7 @@ import { EditUserComponent } from './edit-user/edit-user.component';
     BrowserModule,
     routing,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule
   ],
   providers: [],
--- src/app/edit-user/edit-user.component.ts
+++ src/app/edit-user/edit-user.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { User } from '../user';
 import { UserServiceService } from '../user-service.service';
 import { ActivatedRoute } from '@angular/router';
+import { ReactiveFormsModule, FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-user',
@@ -10,21 +11,47 @@ import { ActivatedRoute } from '@angular/router';
   encapsulation: ViewEncapsulation.None
 })
 export class EditUserComponent implements OnInit {
-  private user: User;
-  private submitted=false;
-  
-  constructor(private userService: UserServiceService, private route: ActivatedRoute) { }
+  submitted = false;
+  userForm: FormGroup;
+  id: string = '';
+  firstName: string = '';
+  lastName: string = '';
+  email: string = '';
+
+  get f() { return this.userForm.controls; }
+
+  constructor(private userService: UserServiceService, private route: ActivatedRoute, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+
     this.userService.getUser(this.route.snapshot.params['id'])
-    .subscribe(data  => { 
-      console.log(data); 
-      this.user = data;  });
+      .subscribe(data => {
+        console.log(data);
+        this.id = data._id;
+        this.userForm.setValue({
+          firstName: data.firstName,
+          lastName: data.lastName,
+          email: data.email
+        });
+      });
+
+    this.userForm = this.formBuilder.group({
+      'firstName': [null, Validators.required],
+      'lastName': [null, Validators.required],
+      'email': [null, [Validators.required, Validators.email]]
+    });
 
   }
-  onSubmit() { 
-    this.submitted = true;  
-    this.userService.updateMember(this.user);
-  }  
+  onSubmit() {
+    this.submitted = true;
+    if (this.userForm.invalid) {
+      return;
+    }
+    else {    
+      const user: User = Object.assign({}, this.userForm.value);
+      user._id=this.id;
+      this.userService.updateMember(user);
+    }
+  }
 
 }
